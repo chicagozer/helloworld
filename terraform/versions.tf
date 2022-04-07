53fd400d47c4d5d00eb52c928b53b5858bb11521
Jim Mittler <chicagozer@gmail.com>
2022-04-07 23:26:44 +0000

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,12 @@
 terraform {
   
-#   backend "remote" {
-#    organization = "rheosoft"
+backend "remote" {
+    organization = "rheosoft"
 
-#    workspaces {
-#      name = "rtdb"
-#    }
-#  }
+    workspaces {
+      prefix = "rtdb-"
+    }
+  }
 
   required_providers {
     helm = {
